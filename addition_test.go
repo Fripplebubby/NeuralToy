14da45401822bdccd03bfbedd0deffdeeb161e9e
Fripplebubby <blitzkriegmop@gmail.com>
2015-09-05 11:15:50 -0400

    Testing module added

---
+++ addition_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+	"math"
+)
+
+func TestGenomes(){
+	toTest := LoadGenome()
+	net := NewNeuralNet(NumInputs, NumOutputs, NumHiddenLayers, NumNeuronsPerHiddenLayer)
+	for i := 0; i < NumTestingRounds; i++ {
+		nextRound := NewRound()
+		net.PutWeights(toTest.weights)
+		result := net.Update(nextRound.Input)
+		answer := 0
+		for i, r := range result {
+			if math.Floor(r + 0.5) == 1 {
+				answer += i
+			}
+		}
+		fmt.Printf("%v + %v = %v\n", nextRound.Int1, nextRound.Int2, nextRound.Answer)
+	}
+}
+
+func LoadGenome() Genome {
+	f, err := ioutil.ReadFile(StrPath + GenomeReadName)
+	if err != nil {
+		panic(err)
+	}
+
+	var weights []float64
+	jsonErr := json.Unmarshal(f, &weights)
+
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+
+	gen := NewGenome(len(weights))
+	gen.weights = weights
+	return gen
+}
